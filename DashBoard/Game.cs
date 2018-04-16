aabd504e7f69a9ec1b6e08c8c552028dea800f59
Marco M. von Hagen <M2vH@users.noreply.github.com>
2018-04-16 10:22:39 +0200

    added StartScreen.png

--- DashBoard/Game.cs
+++ DashBoard/Game.cs
@@ -727,95 +727,95 @@ namespace MonsterHunter
 
         // ToDo: Delete function
         // We don't need parameter;
-        public static void PlayThePlayer(Monster _player)
-        {
-            try
-            {
-                /*  We receive a monster for the player with an existing design
-                 */
-                Game.player = _player;
+        //public static void PlayThePlayer(Monster _player)
+        //{
+        //    try
+        //    {
+        //        /*  We receive a monster for the player with an existing design
+        //         */
+        //        Game.player = _player;
 
-                // In
-                while (Game.play)
-                {
-                    // we check if player is dead
-                    if (Game.playerStats.GetHPoints() <= 0 || Game.player.outfit.stats.GetHPoints() <= 0)
-                    {
-                        // we dont want to run anymore
-                        Game.play = false;
-                        // we stop the countdown
-                        Game.KillCountdown();
+        //        // In
+        //        while (Game.play)
+        //        {
+        //            // we check if player is dead
+        //            if (Game.playerStats.GetHPoints() <= 0 || Game.player.outfit.stats.GetHPoints() <= 0)
+        //            {
+        //                // we dont want to run anymore
+        //                Game.play = false;
+        //                // we stop the countdown
+        //                Game.KillCountdown();
 
-                        // we DONT stop the isEnemy, because his thread will run
-                        // until isEnemy is looser.
-                        //StopEnemy();
+        //                // we DONT stop the isEnemy, because his thread will run
+        //                // until isEnemy is looser.
+        //                //StopEnemy();
 
-                        // dont display a dead player
-                        Game.player.HideMonster(Game.player.pos_x, Game.player.pos_y);
+        //                // dont display a dead player
+        //                Game.player.HideMonster(Game.player.pos_x, Game.player.pos_y);
 
-                        // leave this loop
-                        Game.CloseTheGame();
-                        break;
+        //                // leave this loop
+        //                Game.CloseTheGame();
+        //                break;
 
-                    }
-                    // player is alive
-                    else
-                    {
-                        // we check if he is winner
-                        if (Game.enemyStats.GetHPoints() <= 0)
-                        {
-                            // player has won;
-                            // stop the clock;
-                            Game.KillCountdown();
-                            Game.CloseTheGame();
-                            // leave the 'play'-while
-                            break;
-                        }
-                        //  we need a monster;
-                        //  PrintTheMonster(pos_x, pos_y);
-                        Game.player.PrintMonster(Game.player.pos_x, Game.player.pos_y);
+        //            }
+        //            // player is alive
+        //            else
+        //            {
+        //                // we check if he is winner
+        //                if (Game.enemyStats.GetHPoints() <= 0)
+        //                {
+        //                    // player has won;
+        //                    // stop the clock;
+        //                    Game.KillCountdown();
+        //                    Game.CloseTheGame();
+        //                    // leave the 'play'-while
+        //                    break;
+        //                }
+        //                //  we need a monster;
+        //                //  PrintTheMonster(pos_x, pos_y);
+        //                Game.player.PrintMonster(Game.player.pos_x, Game.player.pos_y);
 
-                        // fight first, then run
-                        lock (Game.printlock)
-                        {
-                            if (Game.dist.distance < 4)
-                            {
-                                Game.player.Fight(Game.player);
-                                // Game.player.HitMonster(Game.playerStats, Game.enemyStats, true);
-                            }
-                        }
+        //                // fight first, then run
+        //                lock (Game.printlock)
+        //                {
+        //                    if (Game.dist.distance < 4)
+        //                    {
+        //                        Game.player.Fight(Game.player);
+        //                        // Game.player.HitMonster(Game.playerStats, Game.enemyStats, true);
+        //                    }
+        //                }
 
-                        int[] nextStep = new int[2];
+        //                int[] nextStep = new int[2];
 
-                        int[] me = new int[2];
-                        me[0] = Game.player.pos_x;
-                        me[1] = Game.player.pos_y;
+        //                int[] me = new int[2];
+        //                me[0] = Game.player.pos_x;
+        //                me[1] = Game.player.pos_y;
 
-                        int[] him = new int[2];
-                        me[0] = Game.enemy.monster.pos_x;
-                        me[1] = Game.enemy.monster.pos_y;
+        //                int[] him = new int[2];
+        //                me[0] = Game.enemy.monster.pos_x;
+        //                me[1] = Game.enemy.monster.pos_y;
 
-                        nextStep = Monster.GetCloser(Game.player, Game.enemy.monster);
+        //                nextStep = Monster.GetCloser(Game.player, Game.enemy.monster);
 
-                        lock (Game.printlock)
-                        {
-                            // Todo: Hier weitermachen
-                            // Check, if we are running out of bounce;
-                            Game.player.HideMonster(me[0], me[1]);
-                            Game.player.pos_x += nextStep[0];
-                            Game.player.pos_y += nextStep[1];
-                        }
-                    } // end of else
-                } // end of while
+        //                lock (Game.printlock)
+        //                {
+        //                    // Hier weitermachen
+        //                    // Check, if we are running out of bounce;
+        //                    Game.player.HideMonster(me[0], me[1]);
+        //                    Game.player.pos_x += nextStep[0];
+        //                    Game.player.pos_y += nextStep[1];
+        //                }
+        //            } // end of else
+        //        } // end of while
 
-            } // end of try
-            catch (ThreadAbortException ex)
-            {
-                System.Diagnostics.Debug.WriteLine("Catch: PlayTheGame " + ex);
-            }
+        //    } // end of try
+        //    catch (ThreadAbortException ex)
+        //    {
+        //        System.Diagnostics.Debug.WriteLine("Catch: PlayTheGame " + ex);
+        //    }
 
 
-        } // end of function
+        //} // end of function
 
         public static Timer startAutoplayTimer;
 
