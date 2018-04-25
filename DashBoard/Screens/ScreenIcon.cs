b8ba2aef3105d2f136ad40212722b634fdd5b37a
Marco M. von Hagen <M2vH@users.noreply.github.com>
2018-04-25 08:24:05 +0200

    added ScreenIcon to make better Icon.ico

---
+++ DashBoard/Screens/ScreenIcon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterHunter.Screens
+{
+    class ScreenIcon
+    {
+        public static void Show()
+        {
+            TextOnScreen.PrintText("TextFiles/Icon.txt");
+
+            Player player = new Player(Game.frodo, "Frodo");
+            player.PrintMonster(30, 10);
+
+
+            InputRequest.PrintInputRequest(ConsoleKey.Enter);
+        }
+    }
+}
